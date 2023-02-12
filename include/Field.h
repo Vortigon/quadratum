cadc1d2852473bae4cee3786d2f5c318c8680793
Andrew Litvinenko <dandeliondevelop@gmail.com>
2023-02-12 19:26:51 +0700

    Added height and width getters

--- include/Field.h
+++ include/Field.h
@@ -4,6 +4,14 @@
 class Field {
 public:
 	Field();
+
+	uint16_t getHeight() const {
+		return height;
+	}
+
+	uint16_t getWidth() const {
+		return width;
+	}
 private:
 	uint16_t height, width;
 };
